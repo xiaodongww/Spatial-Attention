489d6c4055a2d0e2c66082dfc1701e19e9e45b49
Xiaodong Wu <xiaodong.wu.c@gmail.com>
2019-07-04 20:47:01 +0800

    MOD evaluate market1501

--- train_market1501.sh
+++ train_market1501.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
-python main.py -d market -b 48 -j 4 --epochs 100 --log logs/market/ --combine-trainval --step-size 40 --data-dir ./data/market1501
+#python main.py -d market -b 48 -j 4 --epochs 100 --log logs/market/ --combine-trainval --step-size 40 --data-dir ./data/market1501
+#python main.py -d market -b 48 -j 4 --epochs 150 --log logs/market_resume/ --combine-trainval --step-size 40 --data-dir ./data/market1501 --resume logs/market/checkpoint.pth.tar
+python main.py -d market -b 48 -j 4 --epochs 150 --log logs/market_epoch150/ --combine-trainval --step-size 40 --data-dir ./data/market1501
